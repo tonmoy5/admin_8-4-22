27ef0a952b9eb08bb7ecf803cd2c573798e37630
tonmoy5 <tonmoy52532@gmail.com>
2022-04-08 01:51:51 +0600

    pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import Home from "./pages/home/Home";
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
       </header>
+      <body>
+        <Home />
+      </body>
     </div>
   );
 }
---
+++ src/pages/list/List.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./lst.scss"
+
+const List = () => {
+  return (
+    <div>List</div>
+  )
+}
+
+export default List
