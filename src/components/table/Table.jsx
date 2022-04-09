bb0722c14039700d97cef649242f8342bac93a10
tonmoy5 <tonmoy52532@gmail.com>
2022-04-10 00:20:07 +0600

    Homepage Table Done

---
+++ src/components/table/Table.jsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import "./table.scss"
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const List = () => {
+
+  const rows = [
+    {
+      id: 1143155,
+      product: "Acer Nitro 5",
+      img: "https://m.media-amazon.com/images/I/81bc8mA3nKL._AC_UY327_FMwebp_QL65_.jpg",
+      customer: "John Smith",
+      date: "1 March",
+      amount: 785,
+      method: "Cash on Delivery",
+      status: "Approved",
+    },
+    {
+      id: 2235235,
+      product: "Playstation 5",
+      img: "https://m.media-amazon.com/images/I/81bc8mA3nKL._AC_UY327_FMwebp_QL65_.jpg",
+      customer: "Michael Doe",
+      date: "1 March",
+      amount: 900,
+      method: "Online Payment",
+      status: "Pending",
+    },
+    {
+      id: 3235235,
+      product: "Razer Blade Pro",
+      img: "https://m.media-amazon.com/images/I/81bc8mA3nKL._AC_UY327_FMwebp_QL65_.jpg",
+      customer: "John Smith",
+      date: "1 March",
+      amount: 785,
+      method: "Cash on Delivery",
+      status: "Approved",
+    },
+    {
+      id: 4235235,
+      product: "Playstation 5",
+      img: "https://m.media-amazon.com/images/I/81bc8mA3nKL._AC_UY327_FMwebp_QL65_.jpg",
+      customer: "Michael Doe",
+      date: "1 March",
+      amount: 900,
+      method: "Online Payment",
+      status: "Pending",
+    },
+    {
+      id: 5235235,
+      product: "Razer Blade Pro",
+      img: "https://m.media-amazon.com/images/I/81bc8mA3nKL._AC_UY327_FMwebp_QL65_.jpg",
+      customer: "John Smith",
+      date: "1 March",
+      amount: 785,
+      method: "Cash on Delivery",
+      status: "Approved",
+    },
+    {
+      id: 6235235,
+      product: "Playstation 5",
+      img: "https://m.media-amazon.com/images/I/81bc8mA3nKL._AC_UY327_FMwebp_QL65_.jpg",
+      customer: "Michael Doe",
+      date: "1 March",
+      amount: 900,
+      method: "Online Payment",
+      status: "Rejected",
+    }
+  ]
+
+  return (
+    <TableContainer component={Paper} className='table'>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Tracking Id</TableCell>
+            <TableCell className='tableCell'>Product</TableCell>
+            <TableCell className='tableCell'>Customer</TableCell>
+            <TableCell className='tableCell'>Date</TableCell>
+            <TableCell className='tableCell'>Amount</TableCell>
+            <TableCell className='tableCell'>Payment Method</TableCell>
+            <TableCell className='tableCell'>Status</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell className='tableCell'>{row.id}</TableCell>
+              <TableCell className='tableCell'>
+                <div className="cellWrapper">
+                  <img src={row.img} alt={row.product} className="image" />
+                  {row.product}
+                </div>
+              </TableCell>
+              <TableCell className='tableCell'>{row.customer}</TableCell>
+              <TableCell className='tableCell'>{row.date}</TableCell>
+              <TableCell className='tableCell'>{row.amount}</TableCell>
+              <TableCell className='tableCell'>{row.method}</TableCell>
+              <TableCell className='tableCell'>
+                <span className={`status ${row.status}`}>{row.status}</span>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
+
+export default List
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Chart from '../../components/chart/Chart'
 import Featured from '../../components/featured/Featured'
 import Navbar from '../../components/navbar/Navbar'
 import Sidebar from '../../components/sidebar/Sidebar'
+import List from '../../components/table/Table'
 import Widget from '../../components/widget/Widget'
 import "./home.scss"
 
@@ -21,6 +22,10 @@ const Home = () => {
         <div className="charts">
           <Featured />
           <Chart />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <List />
         </div>
       </div>
     </div>
