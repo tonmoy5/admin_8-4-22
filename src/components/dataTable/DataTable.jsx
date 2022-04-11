3848deb419f7784a1ba95b567102f49aab101e93
tonmoy5 <tonmoy52532@gmail.com>
2022-04-11 22:34:22 +0600

    done

--- src/components/dataTable/DataTable.jsx
+++ src/components/dataTable/DataTable.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./dataTable.scss"
 import { DataGrid } from "@mui/x-data-grid"
 import { userColumns, userRows } from '../../datatablesource'
@@ -8,15 +8,21 @@ import { Link } from 'react-router-dom'
 
 const DataTable = () => {
 
+  const [data, setData] = useState(userRows);
+
+  const handleDelete = (id) => {
+    setData(data.filter(row => row.id !== id));
+  }
+
   const actionColumn = [
     {
-      field: "action", headerName: "Action", width: 200, renderCell: () => {
+      field: "action", headerName: "Action", width: 200, renderCell: (params) => {
         return (
           <div className="cellAction">
             <Link to="/users/test" style={{ textDecoration: "none" }}>
               <div className="viewButton">View</div>
             </Link>
-            <div className="deleteButton">Delete</div>
+            <div className="deleteButton" onClick={() => handleDelete(params.row.id)}>Delete</div>
           </div>
         )
       }
@@ -32,7 +38,7 @@ const DataTable = () => {
         </Link>
       </div>
       <DataGrid
-        rows={userRows}
+        rows={data}
         columns={userColumns.concat(actionColumn)}
         pageSize={10}
         checkboxSelection
