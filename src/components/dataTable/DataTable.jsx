cb098eca43af5bf3a5f9cd54572494255909dc18
tonmoy5 <tonmoy52532@gmail.com>
2022-04-17 01:09:32 +0600

    real Time data collect

--- src/components/dataTable/DataTable.jsx
+++ src/components/dataTable/DataTable.jsx
@@ -1,71 +1,105 @@
-import React, { useEffect, useState } from 'react'
-import "./dataTable.scss"
-import { DataGrid } from "@mui/x-data-grid"
-import { userColumns, userRows } from '../../datatablesource'
-import { Link } from 'react-router-dom'
-import { collection, getDocs } from "firebase/firestore";
+import "./dataTable.scss";
+import { DataGrid } from "@mui/x-data-grid";
+import { userColumns, userRows } from "../../datatablesource";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {
+  collection,
+  getDocs,
+  deleteDoc,
+  doc,
+  onSnapshot,
+} from "firebase/firestore";
 import { db } from "../../firebase";
-import { async } from '@firebase/util'
 
-
-const DataTable = () => {
-
+const Datatable = () => {
   const [data, setData] = useState([]);
 
-  useEffect(async () => {
-    const fatchData = async () => {
-      let list = [];
-      try {
-        const querySnapshot = await getDocs(collection(db, "users"));
-        querySnapshot.forEach((doc) => {
+  useEffect(() => {
+    // const fetchData = async () => {
+    //   let list = [];
+    //   try {
+    //     const querySnapshot = await getDocs(collection(db, "users"));
+    //     querySnapshot.forEach((doc) => {
+    //       list.push({ id: doc.id, ...doc.data() });
+    //     });
+    //     setData(list);
+    //     console.log(list);
+    //   } catch (err) {
+    //     console.log(err);
+    //   }
+    // };
+    // fetchData();
+
+    // LISTEN (REALTIME)
+    const unsub = onSnapshot(
+      collection(db, "users"),
+      (snapShot) => {
+        let list = [];
+        snapShot.docs.forEach((doc) => {
           list.push({ id: doc.id, ...doc.data() });
         });
         setData(list);
-        console.log(list);
-      } catch (err) {
-        console.log(err);
+      },
+      (error) => {
+        console.log(error);
       }
-    }
-    fatchData();
-  }, [])
+    );
 
-  const handleDelete = (id) => {
-    setData(data.filter(row => row.id !== id));
-  }
+    return () => {
+      unsub();
+    };
+  }, []);
+
+  const handleDelete = async (id) => {
+    try {
+      await deleteDoc(doc(db, "users", id));
+      setData(data.filter((item) => item.id !== id));
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const actionColumn = [
     {
-      field: "action", headerName: "Action", width: 200, renderCell: (params) => {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: (params) => {
         return (
           <div className="cellAction">
             <Link to="/users/test" style={{ textDecoration: "none" }}>
               <div className="viewButton">View</div>
             </Link>
-            <div className="deleteButton" onClick={() => handleDelete(params.row.id)}>Delete</div>
+            <div
+              className="deleteButton"
+              onClick={() => handleDelete(params.row.id)}
+            >
+              Delete
+            </div>
           </div>
-        )
-      }
-    }
-  ]
-
+        );
+      },
+    },
+  ];
   return (
-    <div className='dataTable'>
+    <div className="datatable">
       <div className="datatableTitle">
-        Add New Users
-        <Link to="/users/new" style={{ textDecoration: "none" }} className='link'>
+        Add New User
+        <Link to="/users/new" className="link">
           Add New
         </Link>
       </div>
       <DataGrid
+        className="datagrid"
         rows={data}
         columns={userColumns.concat(actionColumn)}
-        pageSize={10}
+        pageSize={9}
+        rowsPerPageOptions={[9]}
         checkboxSelection
-        disableSelectionOnClick
-        className='datagrid'
       />
     </div>
-  )
-}
+  );
+};
 
-export default DataTable
+export default Datatable;
